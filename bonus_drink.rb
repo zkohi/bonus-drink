8e561c8a531dfeafe27a90f6c03c3114ff3236ba
Hiroyuki Komatsuzawa <kohiandrew@gmail.com>
2017-06-03 00:53:37 +0900

    Fix BonusDrink class

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,13 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    amount + count_for(amount)
+  end
+
+private
+
+  def self.count_for(amount)
+    sub = (amount / 3)
+    mod = (amount % 3)
+    sub > 0 ? sub + count_for(sub + mod) : 0
   end
 end
