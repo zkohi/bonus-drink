33164ae8730e7900f42b73e638e321fa3f9f065f
Hiroyuki Komatsuzawa <kohiandrew@gmail.com>
2017-06-03 01:23:36 +0900

    Clean BonusDrink spec

--- spec/bonus_drink_spec.rb
+++ spec/bonus_drink_spec.rb
@@ -1,10 +1,29 @@
 require File.expand_path(File.dirname(__FILE__) + '/../bonus_drink')
 
 describe BonusDrink do
-  specify { expect(BonusDrink.total_count_for(0)).to eq 0 }
-  specify { expect(BonusDrink.total_count_for(1)).to eq 1 }
-  specify { expect(BonusDrink.total_count_for(3)).to eq 4 }
-  specify { expect(BonusDrink.total_count_for(5)).to eq 7 }
-  specify { expect(BonusDrink.total_count_for(11)).to eq 16 }
-  specify { expect(BonusDrink.total_count_for(100)).to eq 149 }
+  describe '#total_count_for' do
+    context 'when an amount of bottle is 0' do
+      specify { expect(BonusDrink.total_count_for(0)).to eq 0 }
+    end
+
+    context 'when an amount of bottle is 1' do
+      specify { expect(BonusDrink.total_count_for(1)).to eq 1 }
+    end
+
+    context 'when an amount of bottle is 3' do
+      specify { expect(BonusDrink.total_count_for(3)).to eq 4 }
+    end
+
+    context 'when an amount of bottle is 9' do
+      specify { expect(BonusDrink.total_count_for(9)).to eq 13 }
+    end
+
+    context 'when an amount of bottle is 11' do
+      specify { expect(BonusDrink.total_count_for(11)).to eq 16 }
+    end
+
+    context 'when an amount of bottle is 100' do
+      specify { expect(BonusDrink.total_count_for(100)).to eq 149 }
+    end
+  end
 end
