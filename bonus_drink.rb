96b9cce08d5bdea6aea7d7515fc630dc98232081
Hiroyuki Komatsuzawa <kohiandrew@gmail.com>
2017-06-03 01:09:39 +0900

    Clean BonusDrink class

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,13 +1,13 @@
 class BonusDrink
   def self.total_count_for(amount)
-    amount + count_for(amount)
+    fn = lambda do |n|
+      sub = (n / 3)
+      mod = (n % 3)
+      sub > 0 ? sub + fn.call(sub + mod) : 0
+    end
+    amount + fn.call(amount)
   end
 
 private
 
-  def self.count_for(amount)
-    sub = (amount / 3)
-    mod = (amount % 3)
-    sub > 0 ? sub + count_for(sub + mod) : 0
-  end
 end
